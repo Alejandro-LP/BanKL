a4424640f9d46eb844ffa06c03553aa5eb94227b
AlejandroLP <joral2112@hotmail.com>
2026-04-22 19:43:04 -0500

    Arreglo función de arranque

--- bankl/src/main/java/co/edu/konradlorenz/BanklApplication.java
+++ bankl/src/main/java/co/edu/konradlorenz/BanklApplication.java
@@ -3,10 +3,31 @@ package co.edu.konradlorenz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class BanklApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BanklApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner abrirNavegador() {
+        return args -> {
+            try {
+
+               
+                Thread.sleep(3000);
+
+                
+                new ProcessBuilder("cmd", "/c", "start http://localhost:8080/html/index.html")
+                        .start();
+
+            } catch (Exception e) {
+                System.out.println("No se pudo abrir el navegador automáticamente");
+            }
+        };
     }
 }
